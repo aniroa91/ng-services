8654592f5412106ad2f15350ae58b4d280c14682
huyhoangt2 <01659039498>
2018-08-22 09:48:35 +0700

    local

--- app/profiles/models/PaytvResponse.scala
+++
@@ -1,7 +0,0 @@
-package model.paytv
-
-case class PaytvResponse(
-                         statusContract: Array[((String,String,String),Long)],
-                         nameContract: Array[((String,String),Long)],
-                         provinceContract: Array[((String,String),Long)]
-                       )
