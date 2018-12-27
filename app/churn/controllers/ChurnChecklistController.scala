067a7d0248151444ee45c735bcbf009f72b9ab02
huyhoangt2 <01659039498>
2018-12-27 17:12:33 +0700

    fix page checklist v2

--- app/churn/controllers/ChurnChecklistController.scala
+++ app/churn/controllers/ChurnChecklistController.scala
@@ -30,18 +30,18 @@ class ChurnChecklistController @Inject() (cc: ControllerComponents) extends Abst
 
   def index() =  withAuth {username => implicit request: Request[AnyContent] =>
     val month = CommonService.getPrevMonth()
-    //try {
+    try {
       val rs = ChurnChecklistService.getInternet(null)
       Ok(churn.views.html.checklist.index(rs, month, username, churn.controllers.routes.ChurnChecklistController.index()))
-    /*}
+    }
     catch {
       case e: Exception => Ok(churn.views.html.checklist.index(null, month, username, churn.controllers.routes.ChurnChecklistController.index()))
-    }*/
+    }
   }
 
 
   def getJsonChurn() =  withAuth {username => implicit request: Request[AnyContent] =>
-    //try{
+    try{
         val rs = ChurnChecklistService.getInternet(request)
         val churn1 = Json.obj(
           "cates"        -> rs.ctCheckList.map(x=> x._1),
@@ -113,10 +113,10 @@ class ChurnChecklistController @Inject() (cc: ControllerComponents) extends Abst
           "churn9" -> churn9
         )
         Ok(Json.toJson(json))
-    /*}
+    }
     catch{
       case e: Exception => Ok("Error")
-    }*/
+    }
 
   }
 
